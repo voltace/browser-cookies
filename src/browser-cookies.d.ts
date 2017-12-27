17e01a21ec3735f83fb59e693a1ebf9d3a09b3b0
nino-porcino <nino.porcino@gmail.com>
2017-12-27 18:38:52 +0100

    improve return type for cookies.all()

--- src/browser-cookies.d.ts
+++ src/browser-cookies.d.ts
@@ -8,6 +8,6 @@ export interface CookieOptions {
 export function set(name: string, value: string, options?: CookieOptions): void;
 export function get(name: string): string | null;
 export function erase(name: string, options?: CookieOptions): void;
-export function all(): any;
+export function all(): { [key: string]: string };
 export var defaults: CookieOptions;
 
